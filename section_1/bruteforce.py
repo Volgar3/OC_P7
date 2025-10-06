10e7084bfaad269c39db193f5b82a10aab101477
Volgar3 <h.deles@gmail.com>
2025-10-06 14:03:54 +0200

    Calcul des bénéfices

--- section_1/bruteforce.py
+++ section_1/bruteforce.py
@@ -8,20 +8,27 @@ def extract_data_from_CSV(csv):
     with open(csv_path, newline='') as action_csv:
         return action_csv.read().splitlines()
     
-data = extract_data_from_CSV(csv_path)
 
-actions = []
-for i in data[1:]:
-    action_data = i.split(',')
-    action = { 
-        "name": [action_data[0]],
-        "price": (int(action_data[1])),
-        "benefits": (int(action_data[2].strip().replace('%', '')))
-    }
+def data_treatement():
     
-    actions.append(action)
+    data = extract_data_from_CSV(csv_path)
+    actions = []
+    for i in data[1:]:
+        action_data = i.split(',')
+        action = { 
+            "name": action_data[0],
+            "price": (int(action_data[1])),
+            "benefits_after_2_years": ((int(action_data[1]) * int(action_data[2].strip().replace('%', '')) / 100)) + int(action_data[1])
+        }
+        actions.append(action)
+    return actions
 
+actions = data_treatement()
 
+for action in actions:
+    print(action)
+    
+    
     
     
 
