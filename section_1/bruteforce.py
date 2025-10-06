d7043f8b7f989b1bb1bf8da6aeb1c0120669266e
Aurélien <aurelien.lebray@gmail.com>
2025-10-06 16:25:52 +0200

    Beginned bruteforce

--- section_1/bruteforce.py
+++ section_1/bruteforce.py
@@ -1,5 +1,4 @@
-import time 
-import csv
+from itertools import combinations
 
 csv_path = "excel/liste_action_section_1.csv"
 
@@ -19,12 +18,32 @@ def data_treatement():
             "name": action_data[0],
             "price": (int(action_data[1])),
             "percentage_benefits": int(action_data[2].strip().replace('%', '')),
-            "euros_benefits": ((int(action_data[1]) * int(action_data[2].strip().replace('%', '')) / 100)) + int(action_data[1])
+            "euros_benefits": ((int(action_data[1]) * int(action_data[2].strip().replace('%', '')) / 100))
         }
         actions.append(action)
     return actions
 
-actions = data_treatement()
+def get_combinations(actions, budget):
+    result_comb_total = []
+    for i in range(1, len(actions)):
+        result_comb = combinations(actions, i + 1)
+        result_comb_total.append([item for item in result_comb])
+    
+    for line_comb in result_comb_total:
+        print(len(line_comb))
 
-for action in actions:
-    print(action)
+    for i in range(len(result_comb_total)):
+        total_budget = 0
+        for comb in result_comb_total[i]:
+            total_budget = sum([action['euros_benefits'] for action in comb])
+        if total_budget > 500:
+            result_comb_total[i].pop(comb)
+
+    for line_comb in result_comb_total:
+        print(len(line_comb))
+
+
+actions = data_treatement()
+actions.sort(key=lambda item: item['euros_benefits'], reverse=True)
+budget = 500
+get_combinations(actions, 500)
