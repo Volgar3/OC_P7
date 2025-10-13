27c1661920b58d6c0eaf73a1f7078862a1e11342
Volgar3 <h.deles@gmail.com>
2025-10-13 16:53:24 +0200

    best_comb() nous donnes la meilleure comb

--- section_1/bruteforce.py
+++ section_1/bruteforce.py
@@ -40,15 +40,42 @@ def get_combinations(actions, budget):
             total_budget = sum([action['price'] for action in comb])
             if total_budget <= 500:
                 combinations_filtered.append(comb)
-            result_comb_total[i] = combinations_filtered
-    for line_comb in result_comb_total:
-        print(len(line_comb))
+        result_comb_total[i] = combinations_filtered
         
+    return result_comb_total
+
+def best_combination_for_number_of_actions():
+    ''' Obtenir la meilleure combinaison pour un nombre d'actions données '''
+    result_comb_total = get_combinations(actions, budget)
+    best_comb_per_number_of_actions = []
+    for i in range (len(result_comb_total)):
+        comb_of_action = result_comb_total[i]
+        if comb_of_action: # In case if comb = 0
+            best_comb = max(comb_of_action, key=lambda comb: sum(action['euros_benefits']for action in comb)
+            )
+            best_comb_per_number_of_actions.append(best_comb)
         
+    for best_comb in best_comb_per_number_of_actions:
+        noms = ", ".join(action['name'] for action in best_comb)  # noms des actions
+        prix_total = sum(action['price'] for action in best_comb)
+        benef_total = sum(action['euros_benefits'] for action in best_comb)
+        print(f"{len(best_comb)} actions : {noms} - Prix total: {prix_total} € - Bénéfice total: {benef_total:} €")
+    
+    return best_comb_per_number_of_actions
+
+def best_comb():
+    ''' Give the best combination '''
+    best_combs = best_combination_for_number_of_actions()
+    the_greatesque = max(best_combs, key=lambda comb: sum(action['euros_benefits'] for action in comb))
+    print('--------')
+    print(f"La meilleure combinaison est {the_greatesque}")
+    print('--------')
+
 # --- Initialisation ---
 csv_path = "excel/liste_action_section_1.csv"
 actions = data_treatement()
 actions.sort(key=lambda item: item['euros_benefits'], reverse=True)
 budget = 500
 get_combinations(actions, budget)
+best_comb()
 
