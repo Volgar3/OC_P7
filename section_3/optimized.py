53b6ba29311e5db084049b56f477ff48802e7596
Volgar3 <h.deles@gmail.com>
2026-01-16 19:39:57 +0100

    Same code than section 2 but friendly with float

---
+++ section_3/optimized.py
@@ -0,0 +1,60 @@
+import time
+
+def extract_data_from_CSV(csv_path):
+    with open(csv_path, newline="") as action_csv:
+        return action_csv.read().splitlines()
+
+def data_treatement():
+    data = extract_data_from_CSV(csv_path)
+    actions = []
+    for line in data[1:]:
+        action_data = line.split(",")
+        action = {
+            "name": action_data[0],
+            "price": float(action_data[1]),
+            "percentage_benefits": float(action_data[2].strip().replace("%", "")),
+            "euros_benefits": float(action_data[1]) * float(action_data[2].strip().replace("%", "")) / 100,
+        }
+        actions.append(action)
+    return actions
+
+def sac_a_dos(actions, budget):
+    
+    matrice = [0] * (budget + 1)
+    selected_actions = [ [] for _ in range(budget + 1) ]
+    for action in actions:
+        # Conversion du prix en int pour l'utiliser dans range et comme index
+        price = int(action["price"])
+        benefits = action["euros_benefits"]
+        if price <= 0: continue
+        for combinations in range(budget, price - 1, -1):
+            if matrice[combinations - price] + benefits > matrice[combinations]:
+                matrice[combinations] = matrice[combinations - price] + benefits
+                selected_actions[combinations] = selected_actions[combinations - price] + [action]
+    best_combination = max(range(budget + 1), key=lambda x: matrice[x])
+    return selected_actions[best_combination]
+
+def best_comb():
+    start_time = time.time()
+    best_combination = sac_a_dos(actions, budget)
+    end_time = time.time()
+    
+    names = ", ".join(action["name"] for action in best_combination) 
+    number_of_actions = len(best_combination)
+    total_cost = sum(action["price"] for action in best_combination)
+    total_benefits = sum(action["euros_benefits"] for action in best_combination)
+    
+    print("--------")
+    print(f"Temps d'exécution : {end_time - start_time} secondes")
+    print(f"La meilleure combinaison se compose de {number_of_actions} actions, coûte = {total_cost} € et génère un bénéfice de {total_benefits} après 2 ans. Actions: {names}")
+    print("--------")
+    return best_combination
+
+# --- Initialisation ---
+
+csv_path = "excel/dataset2_Python+P7.csv"
+actions = data_treatement()
+actions.sort(key=lambda item: item["euros_benefits"], reverse=True)
+budget = 500
+
+best_comb()
